5e0f16777b458724b0eb7f34233a6cca6e7047ea
jeftegoesdev <jefte.goes.dev@outlook.com>
2021-05-28 23:36:33 -0300

    Study continuation

---
+++ Section 16/YourFirstUnitTest/Business.Test/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Business.Test
+{
+    [TestFixture]
+    public class SerialPortParserTests
+    {
+        [Test]
+        public void ParsePort_COM1_Returns1()
+        {
+            int result = SerialPortParser.ParsePort("COM1");
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ParsePort_InvalidFormat_ThrowsInvalidFormatException()
+        {
+            TestDelegate action = () => SerialPortParser.ParsePort("1");
+            Assert.Throws<FormatException>(action);
+        }
+    }
+}
---
+++ Section 16/YourFirstUnitTest/Business/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Business
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = SerialPortParser.ParsePort("COM1");
+            Console.WriteLine($"Port: {port}");
+        }
+    }
+}
